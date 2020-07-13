603049f6392961691c1b4320b3f7ca2377bba87c
Prodigy <kkpetkov@gmail.com>
2020-07-13 23:51:08 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Admin on 13.07.2020.
+//  Copyright © 2020 TheProdigy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
